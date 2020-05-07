1f6511c4125cbfc0bde6c8f149bd60387df8ec0d
Kamberos <szymonzuziak@gmail.com>
2020-05-07 23:53:34 +0200

    Import for HostApduService() has been done. Need to check API version - 19 must be

--- Android/app/src/main/java/com/example/nfcmodule/MainActivity.kt
+++ Android/app/src/main/java/com/example/nfcmodule/MainActivity.kt
@@ -6,8 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
-
+import android.nfc.cardemulation.HostApduService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MyHostApduService : HostApduService() {
