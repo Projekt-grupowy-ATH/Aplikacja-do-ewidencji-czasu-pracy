98f2129b2c3484fc9cc341d36e1343eb6c9fb814
Kamberos <szymonzuziak@gmail.com>
2020-08-19 11:31:31 +0200

    Need to check properties inside Project class

--- AttendanceSystem/Models/DBProperties.cs
+++ AttendanceSystem/Models/DBProperties.cs
@@ -5,12 +5,24 @@ using System.Threading.Tasks;
 
 namespace AttendanceSystem.Models
 {
-    public class Users
+    public class User
     {
+        public string UserID { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string Password { get; set; }
         public string Job { get; set; }
         public string TypeOfUser { get; set; }
+    }
+
+    public class Project
+    {
+        public int IDProjektu { get; set; }
+        public string NazwaProjektu { get; set; }
+        public Nullable<int> IDPracownika { get; set; }
+    
+        public virtual Pracownik Pracownik { get; set; }
+        public virtual ICollection<Zadanie> Zadanie { get; set; }
     }
 
 }
