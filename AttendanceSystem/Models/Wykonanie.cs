648d2134e7a1e966c7ca2a32f789c9b2fdf4f2c0
Kamberos <szymonzuziak@gmail.com>
2020-08-21 12:02:22 +0200

    Scaffold-DBContex has been done

---
+++ AttendanceSystem/Models/Wykonanie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AttendanceSystem.Models
+{
+    public partial class Wykonanie
+    {
+        public DateTime PoczZadania { get; set; }
+        public DateTime ZakZadania { get; set; }
+        public int? SumaGodzin { get; set; }
+        public int? Idpracownika { get; set; }
+        public int? Idzadania { get; set; }
+
+        public virtual Pracownik IdpracownikaNavigation { get; set; }
+        public virtual Zadanie IdzadaniaNavigation { get; set; }
+    }
+}
