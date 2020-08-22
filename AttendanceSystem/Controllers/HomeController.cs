fe43f3c9e9a5b11abfb888c5176660f9bea361cb
Kamberos <szymonzuziak@gmail.com>
2020-08-22 14:30:19 +0200

    Test method from controller has been removed

--- AttendanceSystem/Controllers/HomeController.cs
+++ AttendanceSystem/Controllers/HomeController.cs
@@ -20,9 +20,6 @@ namespace AttendanceSystem.Controllers
 
         public IActionResult Index()
         {
-            DBQuerrys a = new DBQuerrys();
-            a.AddNewProject("R&D");
-            a.AddNewTaskToProject("R&D", "Meeting");
             return View();
         }
 
