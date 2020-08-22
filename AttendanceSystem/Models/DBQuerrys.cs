586848e989e9aef94e34edd8a427be0446035d58
Kamberos <szymonzuziak@gmail.com>
2020-08-22 12:25:45 +0200

    Retrieve users list method hass been added

--- AttendanceSystem/Models/DBQuerrys.cs
+++ AttendanceSystem/Models/DBQuerrys.cs
@@ -7,5 +7,25 @@ namespace AttendanceSystem.Models
 {
     public class DBQuerrys
     {
+        readonly EwidencjaContext _connection; 
+        
+        DBQuerrys()
+        {
+            _connection = new EwidencjaContext(); 
+        }
+        public string ShowUsersList()
+        {
+            var item = (from element in _connection.Pracownik
+                select new{
+                    name = element.Imie,
+                    surname = element.Nazwisko,
+                    ID = element.Idpracownika
+                }).ToList();
+            foreach(var a in item)
+            {
+                Console.WriteLine(a);
+            }
+            return item.ToString();
+        }
     }
 }
