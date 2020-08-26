65b70f2993eb5236b67e10861b5e5d32e014bfa2
Kamberos <szymonzuziak@gmail.com>
2020-08-26 16:50:08 +0200

    DBGetQuerrys class with constructor for connection has been created

---
+++ AttendanceSystem/Models/DB/DBGetQuerrys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendanceSystem.Models.DB
+{
+    public class DBGetQuerrys
+    {
+        public readonly EwidencjaContext _connection;
+        public DBGetQuerrys()
+        {
+            _connection = new EwidencjaContext(); 
+        }
+
+    }
+}
