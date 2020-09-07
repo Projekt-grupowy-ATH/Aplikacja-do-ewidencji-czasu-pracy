82ee00412764e59b83c91207aa6405b67ede870d
SzZuziak <szymonzuziak@gmail.com>
2020-09-07 22:43:23 +0200

    AttendanceSystem.Test project has been added to the soultion

---
+++ AttendanceSystem.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AttendanceSystem.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
