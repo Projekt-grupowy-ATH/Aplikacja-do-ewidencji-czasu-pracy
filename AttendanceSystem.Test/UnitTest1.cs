e8b94d3258298cd07efba3ab8e94c2e3e915567f
SzZuziak <szymonzuziak@gmail.com>
2020-09-07 23:51:19 +0200

    moqitem.setupGet need to be changed to the cleaner version

--- AttendanceSystem.Test/UnitTest1.cs
+++ AttendanceSystem.Test/UnitTest1.cs
@@ -11,6 +11,14 @@ namespace AttendanceSystem.Test
         public void test()
         {
             var moqItem = new Mock<EmployeeView>();
+            moqItem.SetupAllProperties();
+            moqItem.SetupGet(p => p.Idpracownika).Returns(1); 
+            moqItem.SetupGet(p => p.Imie).Returns("Adam"); 
+            moqItem.SetupGet(p => p.Nazwisko).Returns("Trzmiel"); 
+            moqItem.SetupGet(p => p.Stanowisko).Returns("IT"); 
+            moqItem.SetupGet(p => p.Uprawnienia).Returns("None"); 
+            moqItem.SetupGet(p => p.Telefon).Returns(111222333); 
+            moqItem.SetupGet(p => p.Email).Returns("adam@example.com"); 
         }
     }
 }
