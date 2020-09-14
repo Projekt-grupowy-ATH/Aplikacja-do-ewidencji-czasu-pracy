adccc5ee835f88c1826abe5d143dc3c6e523a254
paweldrobik <pawel.drobik@outlook.com>
2020-09-14 17:36:11 +0200

    task to projects and employee view of projects

--- AttendanceSystem/Models/DB/DBGetQuerrys.cs
+++ AttendanceSystem/Models/DB/DBGetQuerrys.cs
@@ -30,6 +30,48 @@ namespace AttendanceSystem.Models.DB
             return item;
         }
 
+        public IEnumerable<Projekt> ShowProjectsList()
+        {
+            List<Projekt> projekts = new List<Projekt>();
+
+            var item = (from element in _connection.Projekt
+                        select new Projekt
+                        {
+                            Idprojektu = element.Idprojektu,
+                            NazwaProjektu = element.NazwaProjektu 
+                        }).ToList();
+
+            return item;
+        }
+
+        public IEnumerable<Projekt> ShowUserTask(string email)
+        {
+            int id = ShowUserId(email).Idpracownika;
+
+            List<Projekt> projekts = new List<Projekt>();
+
+            var item = (from element in _connection.Projekt
+                        select new Projekt
+                        {
+                            NazwaProjektu = element.NazwaProjektu,
+                            Idpracownika = element.Idpracownika
+                        }).Where(x => x.Idpracownika == id).ToList();
+
+            return item;
+        }
+        public EmployeeView ShowUserId(string email)
+        {
+            EmployeeView employeeList = new EmployeeView();
+
+            var item = (from element in _connection.Pracownik
+                        select new EmployeeView
+                        {
+                            Idpracownika = element.Idpracownika,
+                            Email = element.Email
+                        }).Where(x => x.Email == email).ToList().FirstOrDefault();
+
+            return item;
+        }
 
     }
 }
---
+++ AttendanceSystem/Models/Properties/TaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendanceSystem.Models.Properties
+{
+    public class TaskViewModel
+    {
+        public IEnumerable<Projekt> Projekts { get; set; }
+
+        public Zadanie Zadanies { get; set; }
+    }
+}
